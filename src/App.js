d3a43c5cd8864a22b11a6c5449513cf3d73ecfd2
LeeVaughn <dlvprogramming@gmail.com>
2021-09-03 09:46:44 -0700

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,6 +7,13 @@ function App() {
         <header>
           <img src="https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/wikipedia-logo2.png" alt="Wikipedia logo" />
         </header>
+
+        <form className="search-bar">
+          <button type="submit" id="search">
+              <i className="fa fa-search"></i>
+          </button>
+          <input type="search" className="search-input" id="searchTerm" placeholder="Search Wikipedia"/>
+        </form>
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
