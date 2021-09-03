4a81844f62bae65773f392c1ed35f2af88fc96b3
LeeVaughn <dlvprogramming@gmail.com>
2021-09-03 15:10:37 -0700

    Add functionality to fetch data from Wikipedia

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ResultsContainer extends Component {
+
+  state = {
+    results: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    this.performSearch("pizza");
+  }
+
+  performSearch(query) {
+    axios.get(`http://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&origin=*&format=json&callback=?`)
+    .then(response => {
+      console.log(response)
+    })
+  }
 
   render() {
     return (
       <div className="output-section">
         <div id="output">
-          
+          results
         </div>
       </div>
     )
