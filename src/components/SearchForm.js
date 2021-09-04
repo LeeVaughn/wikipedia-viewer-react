d2813c522e48fcf4220430becf24b37cc8fc4c01
LeeVaughn <dlvprogramming@gmail.com>
2021-09-04 09:15:20 -0700

    Add search functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchForm extends Component {
+  
+  handleSubmit = (e) => {
+    const { history} = this.props;
+    e.preventDefault();
+    history.push(`/search/${this.query.value}`);
+    e.currentTarget.reset();
+  }
 
   render() {
     return (
-      <form className="search-bar">
+      <form className="search-bar" onSubmit={ this.handleSubmit }>
         <button type="submit" id="search">
             <i className="fa fa-search"></i>
         </button>
-        <input type="search" className="search-input" id="searchTerm" placeholder="Search Wikipedia"/>
+        <input type="search" name="search" ref={(input) => this.query = input} className="search-input" id="searchTerm" placeholder="Search Wikipedia"/>
       </form>
     )
   }
