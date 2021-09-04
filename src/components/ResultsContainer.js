0528f31e0a08083692323bd4ef284a48381e99f4
LeeVaughn <dlvprogramming@gmail.com>
2021-09-04 16:23:21 -0700

    Update componentDidMount and add componentDidUpdate functionality

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -10,14 +10,18 @@ class ResultsContainer extends Component {
   }
 
   componentDidMount() {
-    this.performSearch("pizza");
+    this.performSearch(this.props.data);
+  }
+
+  componentDidUpdate() {
+      this.performSearch(this.props.data);
   }
 
   performSearch(query) {
     axios.get(`http://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&origin=*`)
     .then(response => {
       this.setState({
-        results: response.data,
+        results: response.data[1],
         loading: false
       })
     })
@@ -31,7 +35,7 @@ class ResultsContainer extends Component {
       <div className="output-section">
         <div id="output">
           {
-            (this.state.loading) ? <p>Loading...</p> : <ResultsList data={ this.state.results } />
+            (this.state.loading) ? <p>Loading...</p> : <ResultsList results={ this.state.results } />
           }
         </div>
       </div>
