0199503f18e0b1187c5c0e14b39a4914ae943db5
LeeVaughn <dlvprogramming@gmail.com>
2021-09-04 16:23:58 -0700

    Fix issues with mapping over and displaying data

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,27 +1,25 @@
 // import NoResults from './NoResults';
 
 function ResultsList(props) {
-  const searchResults = props.data.slice(1);
+  const searchResults = props.results;
   let results;
 
   if (searchResults.length) {
-
     results = searchResults.map(result => {
       return (
         <div className="output-results">
-          <a href={ result[3] } target="blank">
-            <h2>{ result[1] }</h2>
-            <p>{ result[2] }</p>
+          <a href={ `https://en.wikipedia.org/wiki/${ result }` } target="blank">
+            <h2>{ result }</h2>
           </a>
         </div>
       )
-    })
+    });
   } else {
     results = <p>No results</p>
   }
 
   return (
-    { results }
+    results
   )
 }
 
